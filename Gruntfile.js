b28e0aa90ea4864ac8bbec8436178d351e673667
holgergp <holgergp@gmail.com>
2015-02-22 16:31:29 +0100

    Preparing for heroku deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -368,7 +368,7 @@ module.exports = function (grunt) {
             ],
             dist: [
                 'copy:styles',
-                'imagemin',
+                //'imagemin',
                 'svgmin'
             ]
         },
